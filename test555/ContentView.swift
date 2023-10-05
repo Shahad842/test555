2c64c3f3e2006f4e14ef01da4a89fccec035fe4f
Shahad <shahad_9215@hotmail.com>
2023-10-05 12:53:23 +0300

    Initial Commit

---
+++ test555/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test555
+//
+//  Created by Shahad Alhothali on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
